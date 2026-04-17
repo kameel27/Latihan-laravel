41c697a613f7799356838ab895772e67075216e3
kameel27 <radencandraka@gmail.com>
2026-04-17 15:28:35 +0700

    pertama kali commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/Halo', function() {return 'Halo, Selamat Datang di Praktikum Laravel';});
+
+Route::get('/user/{id}', function($id){return "Profil User dengan ID :".$id;});
+Route::get('/produk/{slug?}',function($slug='semua-produk'){return "Menampilkan :".$slug;});
+Route::get('/mahasiswa', [MahasiswaController::class,'index']);
